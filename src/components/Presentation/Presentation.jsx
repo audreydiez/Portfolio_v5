661d0aa087ae6742cca31a68a53f4bdd3d1a3a1c
Audrey Diez <audrey.diez@gmail.com>
2022-07-26 23:29:56 +0200

    feat: add frosted glass

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,26 +1,19 @@
-
 import React from 'react'
 import './Presentation.scss'
-import { ReactComponent as Logo } from 'assets/images/logo_gradient.svg'
 
 const Presentation = () => {
-   
-   
     return (
-      <section className="presentation">
-        <div className="presentation__bloc">
-          <h1>Audrey Diez</h1>
-          <p>Développeuse web VueJS, React & Wordpress</p>        
-          <p>Conceptrice UX/UI</p>
-          <p>Mentor & formateur</p>
-          <br />
-          <p className='italic'>Montpellier - Nîmes</p>
-        </div>
-        <div className="presentation__img">
-          <Logo className="presentation__img__logo" />
-        </div>
-      </section>
+        <section className="presentation">
+            <div className="presentation__bloc">
+                <h1>Audrey Diez</h1>
+                <p>Développeuse web VueJS, React & Wordpress</p>
+                <p>Conceptrice UX/UI</p>
+                <p>Mentor & formateur</p>
+                <br />
+                <p className="italic">Montpellier - Nîmes</p>
+            </div>
+        </section>
     )
 }
-  
-export default Presentation 
+
+export default Presentation
