0165f8490f35e78c0c9abc3759e94c9dbaf4ae6d
Audrey Diez <audrey.diez@gmail.com>
2022-09-21 19:15:04 +0200

    feat: presentation

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import './Presentation.scss'
+import Photo from 'assets/images/_tofo.png'
 
 const Presentation = () => {
     return (
         <section className="presentation">
             <div className="presentation__bloc">
-                <h1>Audrey Diez</h1>
-                <p>Développeuse web VueJS, React & Wordpress</p>
-                <p>Conceptrice UX/UI</p>
-                <p>Mentor & formateur</p>
-                <br />
-                <p className="italic">Montpellier - Nîmes</p>
+                <div className="presentation__bloc__img">
+                    <div className="presentation__bloc__img__circle">
+                        <img src={Photo} alt="" />
+                    </div>
+                </div>
+                <div className="presentation__bloc__text">
+                    <div className="above">Développeuse VueJS, React & Wordpress</div>
+                    <p className="above">Montpellier - Nîmes</p>
+                    <h1 className="sub-title">
+                        <span className="name">{Photo}Audrey Diez</span>
+                        <br />
+                        <span className="web">
+                            Création d'applications <br />
+                            et de sites internet.
+                        </span>
+                        <span className="small">Mentor & formatrice.</span>
+                    </h1>
+                </div>
             </div>
         </section>
     )
