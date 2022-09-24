41832733ea86467a36f1c901d7e580e883594c75
Audrey Diez <audrey.diez@gmail.com>
2022-09-25 01:23:22 +0200

    feat: contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,39 @@
 import './Contact.scss'
 import React from 'react'
+import { ReactComponent as Linkedin } from 'assets/images//icons/linkedin.svg'
+import { ReactComponent as Behance } from 'assets/images//icons/behance.svg'
+import { ReactComponent as Git } from 'assets/images//icons/git.svg'
 
 const Contact = () => {
     return (
         <>
             <section className="contact">
-                <h2 className="portfolio__h2">Contact</h2>
+                <h2 className="contact__h2">Contact</h2>
+                <div className="contact__container">
+                    <article className="contact__container__card">
+                        <h3 className="contact__container__card__title">Sur les réseaux</h3>
+                        <div className="contact__container__card__descr social">
+                            <Linkedin className="icon" />
+                            <Behance className="icon" />
+                            <Git className="icon" />
+                        </div>
+                    </article>
+                    <article className="contact__container__card">
+                        <h3 className="contact__container__card">Par email</h3>
+                        <a
+                            href="mailto:audrey.diez@gmail.com"
+                            className="contact__container__card__descr">
+                            audrey.diez@gmail.com
+                        </a>
+                    </article>
+                    <article className="contact__container__card">
+                        <h3 className="contact__container__card">Un café ?</h3>
+                        <p className="contact__container__card__descr">
+                            Rencontrons nous sur Montpellier, Nîmes <br />
+                            ou à mi-chemin !
+                        </p>
+                    </article>
+                </div>
             </section>
         </>
     )
