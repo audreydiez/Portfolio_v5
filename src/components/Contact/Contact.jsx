dab75cec6eec6745fc867c1e26e3c1828278bc6d
Audrey Diez <audrey.diez@gmail.com>
2022-09-26 11:26:50 +0200

    feat: contact links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,36 +6,49 @@ import { ReactComponent as Git } from 'assets/images//icons/git.svg'
 
 const Contact = () => {
     return (
-        <>
-            <section className="contact">
-                <h2 className="contact__h2">Contact</h2>
-                <div className="contact__container">
-                    <article className="contact__container__card">
-                        <h3 className="contact__container__card__title">Sur les réseaux</h3>
-                        <div className="contact__container__card__descr social">
-                            <Linkedin className="icon" />
-                            <Behance className="icon" />
-                            <Git className="icon" />
-                        </div>
-                    </article>
-                    <article className="contact__container__card">
-                        <h3 className="contact__container__card__title">Par email</h3>
+        <section className="contact">
+            <h2 className="contact__h2">Contact</h2>
+            <div className="contact__container">
+                <article className="contact__container__card">
+                    <h3 className="contact__container__card__title">Sur les réseaux</h3>
+                    <div className="contact__container__card__descr social">
                         <a
-                            href="mailto:audrey.diez@gmail.com"
-                            className="contact__container__card__descr">
-                            audrey.diez@gmail.com
+                            href="https://www.linkedin.com/in/audrey-diez-5862345b/"
+                            rel="noopener noreferrer"
+                            target="_blank">
+                            <Linkedin className="icon" />
                         </a>
-                    </article>
-                    <article className="contact__container__card">
-                        <h3 className="contact__container__card__title">Un café ?</h3>
-                        <p className="contact__container__card__descr">
-                            Rencontrons nous sur Montpellier, Nîmes <br />
-                            ou à mi-chemin !
-                        </p>
-                    </article>
-                </div>
-            </section>
-        </>
+                        <a
+                            href="https://www.behance.net/audreydiez736c"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <Behance className="icon" />{' '}
+                        </a>
+                        <a
+                            href="https://github.com/audreydiez"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <Git className="icon" />{' '}
+                        </a>
+                    </div>
+                </article>
+                <article className="contact__container__card">
+                    <h3 className="contact__container__card__title">Par email</h3>
+                    <a
+                        href="mailto:audrey.diez@gmail.com"
+                        className="contact__container__card__descr link">
+                        audrey.diez@gmail.com
+                    </a>
+                </article>
+                <article className="contact__container__card">
+                    <h3 className="contact__container__card__title">Un café ?</h3>
+                    <p className="contact__container__card__descr">
+                        Rencontrons nous sur Montpellier, Nîmes <br />
+                        ou à mi-chemin !
+                    </p>
+                </article>
+            </div>
+        </section>
     )
 }
 
