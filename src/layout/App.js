56562eaabe6a656985235f34abe5e80f4ed96b16
Audrey Diez <audrey.diez@gmail.com>
2022-10-02 01:16:58 +0200

    feat: bubble up

--- src/layout/App.js
+++ src/layout/App.js
@@ -2,7 +2,7 @@ import './App.scss'
 import React, { useState } from 'react'
 import Navbar from 'components/Navbar/Navbar'
 import Presentation from 'components/Presentation/Presentation'
-//import Bubbles from 'components/Bubbles/Bubbles'
+import Bubbles from 'components/Bubbles/Bubbles'
 import ThemeContext from 'layout/themeContext'
 import Services from 'components/Services/Services'
 import Methods from 'components/Methods/Methods'
@@ -18,7 +18,7 @@ function App() {
         <ThemeContext.Provider value={{ themeLight, setThemeLight }}>
             {/* <div> theme is {`${themeLight ? '' : 'not'} themeLight`} </div> */}
             <div id="birds" className="birds">
-                {/* <Bubbles /> */}
+                <Bubbles />
             </div>
             <div className="wrapper">
                 <Navbar />
